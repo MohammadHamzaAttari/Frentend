7eb04a146ba249b3abb086d1d1eb9460b833de50
mohammadhamzaattari <qomaise3@gmail.com>
2023-07-25 00:35:52 +0500

    aded

--- src/components/Constant/url.js
+++ src/components/Constant/url.js
@@ -1,31 +1,34 @@
 export const GETCompanies =
-  "https://fypbankend.azurewebsites.net/api/Companies";
+  "https://backendfinal.azurewebsites.net/api/Companies";
 export const SearchCompanies =
-  "https://fypbankend.azurewebsites.net/api/Companies/search";
+  "https://backendfinal.azurewebsites.net/api/Companies/search";
 export const CrudCompanies =
-  "https://fypbankend.azurewebsites.net/api/Companies/";
-export const GETModels = "https://fypbankend.azurewebsites.net/api/Models";
-export const DeleteModels = "https://fypbankend.azurewebsites.net/api/Models/";
-export const UpdateModels = "https://fypbankend.azurewebsites.net/api/Models/";
-export const CreateTrims = "https://fypbankend.azurewebsites.net/api/Trims";
-export const GETBodies = "https://fypbankend.azurewebsites.net/api/Bodies/";
-export const GETTrims = "https://fypbankend.azurewebsites.net/api/Trims";
-export const UpdateTrims = "https://fypbankend.azurewebsites.net/api/Trims/";
-export const GETAll = "https://fypbankend.azurewebsites.net/api/Companies/All";
+  "https://backendfinal.azurewebsites.net/api/Companies/";
+export const GETModels = "https://backendfinal.azurewebsites.net/api/Models";
+export const DeleteModels =
+  "https://backendfinal.azurewebsites.net/api/Models/";
+export const UpdateModels =
+  "https://backendfinal.azurewebsites.net/api/Models/";
+export const CreateTrims = "https://backendfinal.azurewebsites.net/api/Trims";
+export const GETBodies = "https://backendfinal.azurewebsites.net/api/Bodies/";
+export const GETTrims = "https://backendfinal.azurewebsites.net/api/Trims";
+export const UpdateTrims = "https://backendfinal.azurewebsites.net/api/Trims/";
+export const GETAll =
+  "https://backendfinal.azurewebsites.net/api/Companies/All";
 export const GETUSERS =
-  "https://fypbankend.azurewebsites.net/api/Account/register";
+  "https://backendfinal.azurewebsites.net/api/Account/register";
 export const RegAdmin =
-  "https://fypbankend.azurewebsites.net/api/Account/RegisterAdmin";
+  "https://backendfinal.azurewebsites.net/api/Account/RegisterAdmin";
 export const RegDealer =
-  "https://fypbankend.azurewebsites.net/api/Account/RegisterDealer";
+  "https://backendfinal.azurewebsites.net/api/Account/RegisterDealer";
 export const LoginUser =
-  "https://fypbankend.azurewebsites.net/api/Account/login";
-export const DeleteUser = "https://fypbankend.azurewebsites.net/api/Account/";
-export const Booking = "https://fypbankend.azurewebsites.net/api/Booking";
-export const GETBooking = "https://fypbankend.azurewebsites.net/api/Booking";
-export const GETContact = "https://fypbankend.azurewebsites.net/api/Contact";
-export const GETFeedBack = "https://fypbankend.azurewebsites.net/api/Contact";
+  "https://backendfinal.azurewebsites.net/api/Account/login";
+export const DeleteUser = "https://backendfinal.azurewebsites.net/api/Account/";
+export const Booking = "https://backendfinal.azurewebsites.net/api/Booking";
+export const GETBooking = "https://backendfinal.azurewebsites.net/api/Booking";
+export const GETContact = "https://backendfinal.azurewebsites.net/api/Contact";
+export const GETFeedBack = "https://backendfinal.azurewebsites.net/api/Contact";
 export const DeleteFeedBack =
-  "https://fypbankend.azurewebsites.net/api/Contact/";
+  "https://backendfinal.azurewebsites.net/api/Contact/";
 export const DeleteBooking =
-  "https://fypbankend.azurewebsites.net/api/Booking/";
+  "https://backendfinal.azurewebsites.net/api/Booking/";
